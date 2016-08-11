1cd900dc3d4301adeab90990130e346725bd9e3e
hotdog <hotdog.zyj@duoxieyun.com>
2016-08-11 09:18:19 +0800

    update post.go

--- post.go
+++ post.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/GiterLab/urllib"
+	"github.com/buger/jsonparser"
 )
 
 func DoPost(m map[string]string) (success bool, response string) {
@@ -23,6 +24,25 @@ func DoPost(m map[string]string) (success bool, response string) {
 		fmt.Println("err,", err)
 		return false, str
 	}
+
+	if !check(str) {
+		return false, str
+	}
+
 	return true, str
 
+}
+
+func check(str string) bool {
+	data := []byte(str)
+	err_code, _, _, err := jsonparser.Get(data, "alibaba_aliqin_fc_sms_num_send_response", "result", "err_code")
+	if err != nil {
+		return false
+	}
+
+	if string(err_code) == "0" {
+		return true
+	}
+
+	return false
 }
